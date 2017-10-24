e4cc830c929ded6f153c7632ac76808e6ec4aa52
Dylan Hardy <dylantomhardy@gmail.com>
2017-10-23 18:31:27 -0600

    lol react is tough

---
+++ warm-up/practice/react-calc/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Value from "./main/Value";
+
+function App() {
+    return (
+        <div>
+            <input type="number"/>
+            <span>{Value}</span>
+        </div>
+    )
+}
+
+export default App
---
+++ warm-up/practice/react-calc/src/main/Value.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Value() {
+    <div>
+        value
+    </div>
+}
+
+export default Value;
